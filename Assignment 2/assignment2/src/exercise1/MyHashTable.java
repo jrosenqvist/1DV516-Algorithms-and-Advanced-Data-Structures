a5457b8d0b5a870bd56c35f0937045b27fca7a0c
Joakim Rosenqvist <jr222wb@student.lnu.se>
2019-10-14 23:12:59 +0200

    exercise 1 klar

--- Assignment 2/assignment2/src/exercise1/MyHashTable.java
+++ Assignment 2/assignment2/src/exercise1/MyHashTable.java
@@ -1,60 +1,79 @@
 package exercise1;
 
-import java.util.Arrays;
+import javax.naming.SizeLimitExceededException;
 
 public class MyHashTable<T> implements A2HashTable<T> {
     private Object[] t;
-    private int primes[];
-    private int primesIndex;
-    public int size; //TODO ändra
-    private boolean primeBool[];
+    private int size;
+    private Primes primes;
 
     MyHashTable() {
-        t = new Object[9];        
-        primes = generatePrimes(100000);
-        primesIndex = 0;        
+        t = new Object[19];
+        primes = new Primes();
         size = 0;
-        
     }
 
-    @Override
+    @Override    
     public void insert(T element) {
-        int hash = element.hashCode();
+        int hash = Math.abs(element.hashCode());
         int step = 1;
         int index = hash % t.length;
-        while (t[index] != null) {            
+        while (t[index] != null) {
             index = (hash + (step * step)) % t.length;
-            System.out.println("Kollar index " + index);
-            if ((index + 1) > (t.length / 2)) {
-                System.out.println("Behöver expandera");
-                expandTable();
-            }
             step++;
         }
-        t[index] = element;        
+        t[index] = element;
         size++;
+        if (size >= (t.length / 2)) {
+            System.out.println("Behöver expandera, antal element " + size);
+            try {
+                rehash();
+            } catch (SizeLimitExceededException e) {
+                System.out.println(e);
+            }
+        }
     }
 
     @Override
     public void delete(T element) {
-        // TODO Auto-generated method stub
-        
+        int hash = Math.abs(element.hashCode());
+        int index = hash % t.length;
+        for (int i = 0; i < t.length; i++) {
+            index = (hash + (i * i)) % t.length;
+            if (t[index] == null) continue;
+
+            if (t[index].equals(element)) {
+                t[index] = null;
+                return;
+            }
+        }
     }
 
     @Override
     public boolean contains(T element) {
-        // TODO Auto-generated method stub
+        int hash = Math.abs(element.hashCode());
+        int index = hash % t.length;
+        for (int i = 0; i < t.length; i++) {            
+            index = (hash + (i * i)) % t.length;            
+            if (t[index] == null) continue;
+
+            if (t[index].equals(element)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
-    public int getLengthOfArray() {        
+    public int getLengthOfArray() {
         return t.length;
     }
 
     public void printTable() {
         StringBuilder sb = new StringBuilder();
-        for (Object obj : t) {            
+        for (Object obj : t) {
+            // if (obj == null)
+            // continue;
             sb.append(obj);
             sb.append(", ");
         }
@@ -62,54 +81,53 @@ public class MyHashTable<T> implements A2HashTable<T> {
         System.out.println(sb.toString());
     }
 
-    private void expandTable() {
-        if (primes[primes.length - 1] < 2 * t.length) {
-            generatePrimes(primeBool.length * 10);
-        }        
-        for (int i = primesIndex; i < primes.length; i++) {
-            if (primes[i] > (2 * t.length)) {
-                t = Arrays.copyOf(t, primes[i]);
-                primesIndex = i;                                
-                return;
+    private void rehash() throws SizeLimitExceededException {
+        int newSize = primes.getPrime(t.length * 2);
+
+        @SuppressWarnings("unchecked")
+        T[] old = (T[]) t;
+        
+        t = new Object[newSize];
+        size = 0;
+                
+        for (int i = 0; i < old.length; i++) {
+            if (old[i] != null) {                  
+                insert(old[i]);                
             }            
         }        
     }
 
     // Sieve of Eratosthenes
-    private int[] generatePrimes(int n) {
-        int startIndex;
-        if (primeBool == null) {
-            primeBool = new boolean[n + 1]; 
-            for(int i = 0 ; i < n; i++) 
-                primeBool[i] = true; 
-            startIndex = 2;
+    private class Primes {
+        private boolean[] sieve;
+
+        Primes() {
+            sieve = generateSieve(1000000);
         }
-        else {
-            startIndex = primeBool.length - 1;
-            primeBool = Arrays.copyOf(primeBool, n);
-            for (int i = startIndex; i < n; i++)
-                primeBool[i] = true;
-        }        
-          
-        for(int p = startIndex; p * p <= n; p++) 
-        {             
-            if(primeBool[p] == true) 
-            {                 
-                for(int i = p * p; i <= n; i += p) 
-                    primeBool[i] = false; 
-            } 
-        }
-        int tmp[] = new int[10];
-        int tmpIndex = 0;
-        for (int i = 2; i <= n; i++) {
-            if (primeBool[i] == true) {                
-                if (tmpIndex >= tmp.length - 1) {
-                    tmp = Arrays.copyOf(tmp, tmp.length * 2);
+
+        int getPrime(int largerThan) throws SizeLimitExceededException {
+            for (int i = largerThan; i < sieve.length; i++) {
+                if (sieve[i] == true) {
+                    System.out.println("Ska vara större än " + largerThan + ", hittade " + i);
+                    return i;
                 }
-                tmp[tmpIndex++] = i;                
-            }        
+            }
+            throw new SizeLimitExceededException("Cannot find a new larger prime.");
         }
-            
-        return tmp;
+
+        private boolean[] generateSieve(int size) {
+            boolean[] prime = new boolean[size + 1];
+            for (int i = 0; i < size; i++) {
+                prime[i] = true;
+            }
+            for (int p = 2; p * p <= size; p++) {
+                if (prime[p] == true) {
+                    for (int i = p * p; i <= size; i += p) {
+                        prime[i] = false;
+                    }
+                }
+            }
+            return prime;
+        }
     }
 }
--- Assignment 2/assignment2/src/exercise1/Program.java
+++ Assignment 2/assignment2/src/exercise1/Program.java
@@ -4,17 +4,32 @@ import java.util.Random;
 
 public class Program {
     public static void main(String[] args) {
-        MyHashTable<Integer> mht = new MyHashTable<Integer>();
-        mht.insert(3);
-        mht.insert(5);
-        mht.insert(7);
-        mht.insert(2);
+        MyHashTable<Integer> mht = new MyHashTable<Integer>();        
+        int inserted[] = new int[15000];
         Random r = new Random();
-        for (int n = 1; n < 100; n++) {
-            mht.insert(r.nextInt(1000));
+        int count = 0;
+        for (int i = 0; i < inserted.length; i++) {            
+            int n = r.nextInt(inserted.length / 10);         
+            mht.insert(n);
+            inserted[i] = n;
+            count++;            
+        }               
+        count = 0;
+        for (int i = 0; i < inserted.length; i++) {
+             int n = inserted[r.nextInt(inserted.length - 1)];
+             if (mht.contains(n)) count++;             
         }
-        mht.printTable();
-        System.out.println(mht.size);
+        System.out.println("Hittade " + count)        ;
+        System.out.println(mht.getLengthOfArray());
         
+        
+        MyHashTable<String> mht2 = new MyHashTable<String>();               
+        mht2.insert("Hejsan");
+        mht2.insert("Hejsan");        
+        System.out.println(mht2.contains("Hejsan"));
+        mht2.delete("Hejsan");
+        System.out.println(mht2.contains("Hejsan"));
+        mht2.delete("Hejsan");
+        System.out.println(mht2.contains("Hejsan"));
     }
 }
