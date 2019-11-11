c5784c91849dc748a7a995e9874ccdf1cb201dee
Joakim Rosenqvist <jr222wb@student.lnu.se>
2019-11-11 15:51:14 +0100

    tjohej

--- Assignment 3/src/exercise1/MyUndirectedGraph.java
+++ Assignment 3/src/exercise1/MyUndirectedGraph.java
@@ -8,37 +8,40 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Stack;
 
-public class MyUndirectedGraph<T> implements A3Graph<T> {    
+public class MyUndirectedGraph<T> implements A3Graph<T> {
     private List<Vertex> vertices = new ArrayList<>();
-    
+
     @Override
     public void addVertex(T vertex) {
-        vertices.add(new Vertex(vertex));        
+        vertices.add(new Vertex(vertex));
     }
 
     @Override
     public void addEdge(T sourceVertex, T targetVertex) throws NoSuchElementException {
         Vertex source = null;
         Vertex target = null;
-        for (Vertex v : vertices) {            
-            if (v.id == sourceVertex) source = v;
-            if (v.id == targetVertex) target = v;
-            if (source != null && target != null) break;
+        for (Vertex v : vertices) {
+            if (v.id == sourceVertex)
+                source = v;
+            if (v.id == targetVertex)
+                target = v;
+            if (source != null && target != null)
+                break;
         }
-        if (source == null || target == null) {            
+        if (source == null || target == null) {
             throw new NoSuchElementException();
         }
         Edge e = new Edge(source, target);
         source.adjacent.add(e);
-        target.adjacent.add(e);        
+        target.adjacent.add(e);
     }
-    
+
     // TODO fixa den här
     @Override
-    public boolean isConnected() {        
+    public boolean isConnected() {
         HashSet<Vertex> visited = new HashSet<>();
 
-        connectedHelper(vertices.get(0), visited);        
+        connectedHelper(vertices.get(0), visited);
 
         return visited.size() == vertices.size();
     }
@@ -49,7 +52,7 @@ public class MyUndirectedGraph<T> implements A3Graph<T> {
             Vertex target = e.source == v ? e.target : e.source;
             if (!visited.contains(target))
                 connectedHelper(target, visited);
-        }        
+        }
     }
 
     @Override
@@ -57,23 +60,22 @@ public class MyUndirectedGraph<T> implements A3Graph<T> {
         HashSet<Vertex> visited = new HashSet<>();
         for (int i = 0; i < vertices.size(); i++) {
             Vertex current = vertices.get(i);
-            if (!visited.contains(current)) 
+            if (!visited.contains(current))
                 if (findCycle(current, visited, null))
                     return false;
-        }         
+        }
         return true;
     }
 
     private boolean findCycle(Vertex v, HashSet<Vertex> visited, Vertex parent) {
         visited.add(v);
-        
+
         for (Edge e : v.adjacent) {
             Vertex u = e.source == v ? e.target : e.source;
             if (!visited.contains(u)) {
                 if (findCycle(u, visited, v))
                     return true;
-            }
-            else if (u != parent)
+            } else if (u != parent)
                 return true;
         }
         return false;
@@ -88,10 +90,10 @@ public class MyUndirectedGraph<T> implements A3Graph<T> {
                 connected.add(u);
             }
         }
-        
+
         List<List<T>> listOfLists = new ArrayList<>();
         List<T> list = new ArrayList<>();
-        connected.forEach(vertex -> list.add(vertex.id) );
+        connected.forEach(vertex -> list.add(vertex.id));
         listOfLists.add(list);
         vertices.forEach(vertex -> {
             if (!connected.contains(vertex)) {
@@ -106,19 +108,19 @@ public class MyUndirectedGraph<T> implements A3Graph<T> {
 
     @Override
     public boolean hasEulerPath() {
-        // if (!isConnected()) 
-        //     return false;
+        // if (!isConnected())
+        // return false;
 
         // TODO fixa så att eulerpath kan detektera lösa noder osv
         int odds = 0;
 
         for (Vertex v : vertices) {
-            int degree = v.adjacent.size();             
+            int degree = v.adjacent.size();
             if (degree % 2 != 0)
-                odds++;            
+                odds++;
         }
-        
-        if (odds == 2 || odds == 0) 
+
+        if (odds == 2 || odds == 0)
             return true;
 
         return false;
@@ -126,14 +128,14 @@ public class MyUndirectedGraph<T> implements A3Graph<T> {
 
     @Override
     public List<T> eulerPath() {
-        Vertex start = null;        
-        HashSet<Edge> used = new HashSet<>();        
+        Vertex start = null;
+        HashSet<Edge> used = new HashSet<>();
         Stack<Vertex> stack = new Stack<>();
         LinkedList<Vertex> path = new LinkedList<>();
 
         for (Vertex v : vertices) {
             if (v.adjacent.size() % 2 != 0)
-                start = v;            
+                start = v;
         }
         if (start == null)
             start = vertices.get(0);
@@ -143,18 +145,19 @@ public class MyUndirectedGraph<T> implements A3Graph<T> {
         while (!stack.isEmpty()) {
             Vertex v = stack.pop();
             Iterator<Edge> adjacent = v.adjacent.iterator();
-            
+
             while (adjacent.hasNext()) {
                 Edge edge = adjacent.next();
-                if (used.contains(edge))
-                    continue;
-                used.add(edge);
-                stack.push(v);
-                v = edge.source == v ? edge.target : edge.source;
-                adjacent = v.adjacent.iterator();
+
+                if (!used.contains(edge)) {
+                    used.add(edge);
+                    stack.push(v);
+                    v = edge.source == v ? edge.target : edge.source;
+                    adjacent = v.adjacent.iterator();
+                }
             }
             path.addFirst(v);
-        }        
+        }
         List<T> list = new ArrayList<>();
         path.forEach(vertex -> list.add(vertex.id));
         return list;
---
+++ Assignment 3/src/exercise3/MySocialNetwork.java
@@ -0,0 +1,26 @@
+package exercise3;
+
+import java.util.List;
+import exercise1.*;
+
+public class MySocialNetwork<T> extends MyUndirectedGraph<T> implements A3SocialNetwork<T> {
+
+    @Override
+    public int numberOfPeopleAtFriendshipDistance(T vertex, int distance) {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    @Override
+    public int furthestDistanceInFriendshipRelationships(T vertex) {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    @Override
+    public List<T> possibleFriends(T vertex) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
